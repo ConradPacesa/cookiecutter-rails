5a0672a5de4d771e78a0db8886971027be03c88d
Conrad Pacesa <conradpacesa@gmail.com>
2018-06-18 23:54:54 -0400

    fix typo

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,7 +2,7 @@ import os
 
 app_name = '{{cookiecutter.app_name}}'
 
-if '{{cookicutter.use_docker}}'.lower() != 'y':
+if '{{cookiecutter.use_docker}}'.lower() != 'y':
   os.system('rails new .')
 else: 
   os.system('docker-compose run web rails new . --force --database=postgresql')
