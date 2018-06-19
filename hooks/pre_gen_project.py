92918c58432b59035efdcf872434bddf37f6021e
Conrad Pacesa <conradpacesa@gmail.com>
2018-06-18 23:25:52 -0400

    Remove shell script

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,6 +2,6 @@ import os
 
 app_name = '{{cookiecutter.app_name}}'
 
-shell_script = 'cd ../ && sh ./generate_app.sh {}'.format(app_name)
+shell_script = 'rails new {}'.format(app_name)
 
 os.system(shell_script)
