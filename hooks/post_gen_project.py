676fb37d400e847e0c69420a69379a08b1b6558a
Conrad Pacesa <conradpacesa@gmail.com>
2018-06-19 21:01:10 -0400

    Added more docker commands

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,29 @@
 import os
 
-app_name = '{{cookiecutter.app_name}}'
+database = """
+default: &default
+  adapter: postgresql
+  encoding: unicode
+  host: db
+  username: postgres
+  password:
+  pool: 5
+
+development:
+  <<: *default
+  database: myapp_development
+
+
+test:
+  <<: *default
+  database: myapp_test"""
 
 if '{{cookiecutter.use_docker}}'.lower() == 'y':
   os.system('docker-compose run web rails new . --force --database=postgresql')
+  os.system('docker-compose build')
+  with open('./config/database.yml', 'w') as f:
+    f.write(database)
+  f.close()
+  os.system('docker-compose up')
 else: 
   os.system('rails new .')
